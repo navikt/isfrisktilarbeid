637495973165ff079b3a739deb0ed3e36fdad2d4
Anders Rognstad <79838644+andersrognstad@users.noreply.github.com>
2024-04-02 13:12:11 +0200

    IS-2213: Implement pdfgenclient (#18)

---
+++ src/main/kotlin/no/nav/syfo/infrastructure/clients/pdfgen/PdfModel.kt
@@ -0,0 +1,48 @@
+package no.nav.syfo.infrastructure.clients.pdfgen
+
+import no.nav.syfo.domain.DocumentComponent
+import no.nav.syfo.domain.sanitizeForPdfGen
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.util.*
+
+sealed class PdfModel private constructor(
+    val mottakerFodselsnummer: String?,
+    val mottakerNavn: String,
+    val datoSendt: String,
+    val documentComponents: List<DocumentComponent>,
+) {
+    private constructor(
+        mottakerFodselsnummer: String?,
+        mottakerNavn: String,
+        documentComponents: List<DocumentComponent>
+    ) : this(
+        mottakerFodselsnummer = mottakerFodselsnummer,
+        mottakerNavn = mottakerNavn,
+        datoSendt = LocalDate.now().format(formatter),
+        documentComponents = documentComponents.sanitizeForPdfGen()
+    )
+
+    class VedtakPdfModel(
+        mottakerFodselsnummer: String,
+        mottakerNavn: String,
+        documentComponents: List<DocumentComponent>,
+    ) : PdfModel(
+        mottakerFodselsnummer = mottakerFodselsnummer,
+        mottakerNavn = mottakerNavn,
+        documentComponents = documentComponents,
+    )
+
+    class BehandlerMeldingPdfModel(
+        mottakerNavn: String,
+        documentComponents: List<DocumentComponent>,
+    ) : PdfModel(
+        mottakerFodselsnummer = null,
+        mottakerNavn = mottakerNavn,
+        documentComponents = documentComponents,
+    )
+
+    companion object {
+        private val formatter = DateTimeFormatter.ofPattern("dd. MMMM yyyy", Locale("no", "NO"))
+    }
+}
--- src/test/kotlin/no/nav/syfo/infrastructure/clients/pdfgen/PdfGenClientSpek.kt
+++ src/test/kotlin/no/nav/syfo/infrastructure/clients/pdfgen/PdfGenClientSpek.kt
@@ -3,6 +3,7 @@ package no.nav.syfo.infrastructure.clients.pdfgen
 import kotlinx.coroutines.runBlocking
 import no.nav.syfo.ExternalMockEnvironment
 import no.nav.syfo.UserConstants
+import no.nav.syfo.generator.generateDocumentComponent
 import org.amshove.kluent.shouldBeEqualTo
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.specification.describe
@@ -16,11 +17,29 @@ class PdfGenClientSpek : Spek({
             val pdf = runBlocking {
                 pdfGenClient.createVedtakPdf(
                     callId = "",
-                    payload = "",
+                    payload = PdfModel.VedtakPdfModel(
+                        mottakerFodselsnummer = UserConstants.ARBEIDSTAKER_PERSONIDENT.value,
+                        mottakerNavn = UserConstants.PERSON_FULLNAME_DASH,
+                        documentComponents = generateDocumentComponent("Litt fritekst"),
+                    )
                 )
             }
 
             pdf shouldBeEqualTo UserConstants.PDF_VEDTAK
+        }
+
+        it("returns bytearray of pdf for melding til behandler") {
+            val pdf = runBlocking {
+                pdfGenClient.createBehandlerPdf(
+                    callId = "",
+                    payload = PdfModel.BehandlerMeldingPdfModel(
+                        mottakerNavn = UserConstants.PERSON_FULLNAME_DASH,
+                        documentComponents = generateDocumentComponent("Litt fritekst"),
+                    )
+                )
+            }
+
+            pdf shouldBeEqualTo UserConstants.PDF_BEHANDLER
         }
     }
 })
