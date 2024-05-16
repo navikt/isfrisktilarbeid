d6076f1f3879798415ca8b840b35471cbd8404c6
geir-waagboe <geir.arne.waagbo@nav.no>
2024-05-16 15:22:20 +0200

    Just for testing

--- src/main/kotlin/no/nav/syfo/infrastructure/mq/InfotrygdMQSender.kt
+++ src/main/kotlin/no/nav/syfo/infrastructure/mq/InfotrygdMQSender.kt
@@ -1,7 +1,6 @@
 package no.nav.syfo.infrastructure.mq
 
 import com.ibm.mq.jms.MQDestination
-import com.ibm.msg.client.wmq.WMQConstants
 import com.ibm.msg.client.wmq.common.CommonConstants
 import io.micrometer.core.instrument.Counter
 import no.nav.syfo.infrastructure.metric.METRICS_NS
@@ -38,13 +37,16 @@ class InfotrygdMQSender(
             (destination as MQDestination).messageBodyStyle = CommonConstants.WMQ_MESSAGE_BODY_MQ
             (destination as MQDestination).setBooleanProperty(CommonConstants.WMQ_MQMD_WRITE_ENABLED, true)
             val message = context.createTextMessage(payload)
-           /* val customMessageId = ByteArray(24)
+           /*
+            val customMessageId = ByteArray(24)
             for (i in 0..23) {
                 // Hex-string 010203040506070801020304050607080102030405060708
                 customMessageId[i] = ((i % 8) + 1).toByte()
             }
             message.setObjectProperty(WMQConstants.JMS_IBM_MQMD_MSGID, customMessageId)
-            message.jmsCorrelationID = "modiatest2"*/
+            message.jmsCorrelationID = "modiatest2"
+
+            */
             context.createProducer().send(destination, message)
             log.info("Sent message to MQ, msgId: ${message.jmsMessageID}, correlationId: ${message.jmsCorrelationID} payload: $payload")
         }
