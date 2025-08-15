bc8f63ee71ec52f5ce7247818de99adbfc45c90a
geir-waagboe <geir.arne.waagbo@nav.no>
2025-08-15 16:33:45 +0200

    IS-3384: Inkluder journalforing og gosys i dto

--- src/main/kotlin/no/nav/syfo/api/endpoints/VedtakEndpoints.kt
+++ src/main/kotlin/no/nav/syfo/api/endpoints/VedtakEndpoints.kt
@@ -119,7 +119,6 @@ fun Route.registerVedtakEndpoints(
                 val vedtak = vedtakService.getVedtak(uuid = newVedtak.uuid)
                 log.info("Created vedtak with infotrygd status: ${vedtak.infotrygdStatus}")
                 call.respond(HttpStatusCode.Created, VedtakResponseDTO.createFromVedtak(vedtak = vedtak))
-
             }
         }
         put(ferdigbehandlingPath) {
