5e2b28bf5445b4002dacf5129510bafba3f304fa
Solgård <vetle.bu.solgard@nav.no>
2025-12-03 14:44:25 +0100

    IS: Add documentation on VedtakStatus

--- src/main/kotlin/no/nav/syfo/domain/VedtakStatus.kt
+++ src/main/kotlin/no/nav/syfo/domain/VedtakStatus.kt
@@ -4,6 +4,14 @@ import no.nav.syfo.util.nowUTC
 import java.time.OffsetDateTime
 import java.util.*
 
+/**
+ * VedtakStatus representerer en status for et vedtak for § 8-5 friskmelding til arbeidsformidling.
+ *
+ * Det finnes to typer statuser:
+ * - `FATTET` - Når selve vedtaket er fattet av en veileder. Det kan likevel være videre prosesser etter at man fatter et vedtak som gjør at man som veileder ønsker å fortsette å se vedtaket som en aktiv oppgave, f.eks. overgang til arbeidsrettet oppfølging, koordinering med ny veileder eller videre oppfølging av sykmeldt.
+ * - `FERDIG_BEHANDLET` - Når et vedtak er ferdigbehandlet av veileder. Betyr at veileder er ferdig med behandlingen av vedtaket. Vedtaket vil da ikke lenger være en aktiv oppgave for veileder.
+ *
+ */
 data class VedtakStatus private constructor(
     val uuid: UUID,
     val createdAt: OffsetDateTime,
