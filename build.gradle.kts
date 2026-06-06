46f9bc4bbb06b54923a3f955ac2280dc966b4355
Håkon Salomonsen Møller <hakon.salomonsen.moller@nav.no>
2026-06-06 11:18:21 +0200

    Simplify maven repository credentials block

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,8 +35,8 @@ repositories {
     maven {
         url = uri("https://maven.pkg.github.com/navikt/isyfo-backend-common")
         credentials {
-            username = project.findProperty("githubUser") as String? ?: "x-access-token"
-            password = project.findProperty("githubPassword") as String? ?: System.getenv("GITHUB_TOKEN")
+            username = providers.gradleProperty("githubUser").orNull
+            password = providers.gradleProperty("githubPassword").orNull
         }
     }
 }
