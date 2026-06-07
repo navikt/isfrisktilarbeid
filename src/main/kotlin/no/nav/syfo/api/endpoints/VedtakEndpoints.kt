88024ab41b45fc1532bfba4e374d8d1c1c4864f9
Håkon Salomonsen Møller <hakon.salomonsen.moller@nav.no>
2026-06-07 13:06:28 +0200

    Bump isyfoBackendCommonVersion to 0.0.46 and refactor access checks in VedtakEndpoints

--- src/main/kotlin/no/nav/syfo/api/endpoints/VedtakEndpoints.kt
+++ src/main/kotlin/no/nav/syfo/api/endpoints/VedtakEndpoints.kt
@@ -36,35 +36,29 @@ fun Route.registerVedtakEndpoints(
 
     route(apiBasePath) {
         get(vilkarPath) {
-            val action = "get vilkar for person"
-
             checkPersonAndSyfoTilgang(
-                action = action,
+                action = "get vilkar for person",
                 tilgangskontrollClient = tilgangskontrollClient,
-            ) { authorizedContext ->
-                with(authorizedContext) {
-                    val personident = Personident(personIdent.value)
+            ) { authorizedUser, targetPersonIdent, callId ->
+                val personident = Personident(targetPersonIdent.value)
 
-                    val isArbeidssoker =
-                        arbeidssokeroppslagClient.isArbeidssoker(
-                            callId,
-                            personident,
-                            token
-                        )
+                val isArbeidssoker =
+                    arbeidssokeroppslagClient.isArbeidssoker(
+                        callId,
+                        personident,
+                        authorizedUser.token
+                    )
 
-                    call.respond(HttpStatusCode.OK, VilkarResponseDTO(isArbeidssoker))
-                }
+                call.respond(HttpStatusCode.OK, VilkarResponseDTO(isArbeidssoker))
             }
         }
 
         get(vedtakPath) {
-            val action = "get vedtak for person"
-
             checkPersonAndSyfoTilgang(
-                action = action,
+                action = "get vedtak for person",
                 tilgangskontrollClient = tilgangskontrollClient,
-            ) { authorized ->
-                val personident = Personident(authorized.personIdent.value)
+            ) { _, targetPersonIdent, _ ->
+                val personident = Personident(targetPersonIdent.value)
 
                 val vedtak = vedtakService.getVedtak(personident = personident)
                 val responseDTO = vedtak.map { VedtakResponseDTO.createFromVedtak(it) }
@@ -74,97 +68,92 @@ fun Route.registerVedtakEndpoints(
         }
 
         post(vedtakPath) {
-            val action = "create vedtak"
             val log = call.application.log
 
             checkPersonAndSyfoTilgang(
-                action = action,
+                action = "create vedtak",
                 tilgangskontrollClient = tilgangskontrollClient,
                 requiresWriteAccess = true,
-            ) { authorizedContext ->
-                with(authorizedContext) {
-                    val personident = Personident(personIdent.value)
+            ) { authorizedUser, targetPersonIdent, callId ->
+                val personident = Personident(targetPersonIdent.value)
 
-                    val requestDTO = call.receive<VedtakRequestDTO>()
-                    if (requestDTO.begrunnelse.isBlank() || requestDTO.document.isEmpty()) {
-                        throw IllegalArgumentException("Vedtak can't have an empty begrunnelse or document")
-                    }
-                    if (requestDTO.tom.isBefore(requestDTO.fom)) {
-                        throw IllegalArgumentException("Tildato i vedtak kan ikke være før fradato.")
-                    }
+                val requestDTO = call.receive<VedtakRequestDTO>()
+                if (requestDTO.begrunnelse.isBlank() || requestDTO.document.isEmpty()) {
+                    throw IllegalArgumentException("Vedtak can't have an empty begrunnelse or document")
+                }
+                if (requestDTO.tom.isBefore(requestDTO.fom)) {
+                    throw IllegalArgumentException("Tildato i vedtak kan ikke være før fradato.")
+                }
 
-                    val existingVedtak = vedtakService.getVedtak(personident)
-                    val currentVedtak = existingVedtak.firstOrNull()
-                    if (existingVedtak.any { !it.isFerdigbehandlet() }) {
-                        call.respond(HttpStatusCode.Conflict, "Finnes allerede et åpent vedtak for personen")
-                    } else if (currentVedtak != null && currentVedtak.tom.isAfter(requestDTO.fom)) {
-                        log.warn("Forsøker å opprette vedtak som overlapper med et tidligere vedtak")
-                        call.respond(HttpStatusCode.Conflict, "Vedtaksperioden overlapper med et tidligere vedtak")
-                    } else if (!arbeidssokeroppslagClient.isArbeidssoker(
-                            callId,
-                            personident,
-                            token
-                        )
-                    ) {
-                        log.warn("Forsøker å opprette vedtak for person som ikke er registrert som arbeidssøker")
-                        call.respond(HttpStatusCode.BadRequest, "Personen er ikke registrert som arbeidssøker")
-                    } else {
-                        val (newVedtak, pdf) = vedtakService.createVedtak(
-                            personident = personident,
-                            veilederident = navIdent.value,
-                            begrunnelse = requestDTO.begrunnelse,
-                            document = requestDTO.document,
-                            fom = requestDTO.fom,
-                            tom = requestDTO.tom,
-                            callId = callId,
-                        )
-                        coroutineScope.launch {
-                            try {
-                                val journalfortVedtak = vedtakService.journalforVedtak(newVedtak, pdf).getOrThrow()
-                                if (journalfortVedtak.isJournalfort()) {
-                                    vedtakService.createGosysOppgaveForVedtak(journalfortVedtak)
-                                }
-                            } catch (exc: Exception) {
-                                log.error("Journalforing eller gosysoppgave feilet, cronjob vil forsøke på nytt", exc)
+                val existingVedtak = vedtakService.getVedtak(personident)
+                val currentVedtak = existingVedtak.firstOrNull()
+                if (existingVedtak.any { !it.isFerdigbehandlet() }) {
+                    call.respond(HttpStatusCode.Conflict, "Finnes allerede et åpent vedtak for personen")
+                } else if (currentVedtak != null && currentVedtak.tom.isAfter(requestDTO.fom)) {
+                    log.warn("Forsøker å opprette vedtak som overlapper med et tidligere vedtak")
+                    call.respond(HttpStatusCode.Conflict, "Vedtaksperioden overlapper med et tidligere vedtak")
+                } else if (!arbeidssokeroppslagClient.isArbeidssoker(
+                        callId,
+                        personident,
+                        authorizedUser.token
+                    )
+                ) {
+                    log.warn("Forsøker å opprette vedtak for person som ikke er registrert som arbeidssøker")
+                    call.respond(HttpStatusCode.BadRequest, "Personen er ikke registrert som arbeidssøker")
+                } else {
+                    val (newVedtak, pdf) = vedtakService.createVedtak(
+                        personident = personident,
+                        veilederident = authorizedUser.navIdent.value,
+                        begrunnelse = requestDTO.begrunnelse,
+                        document = requestDTO.document,
+                        fom = requestDTO.fom,
+                        tom = requestDTO.tom,
+                        callId = callId,
+                    )
+                    coroutineScope.launch {
+                        try {
+                            val journalfortVedtak = vedtakService.journalforVedtak(newVedtak, pdf).getOrThrow()
+                            if (journalfortVedtak.isJournalfort()) {
+                                vedtakService.createGosysOppgaveForVedtak(journalfortVedtak)
                             }
+                        } catch (exc: Exception) {
+                            log.error("Journalforing eller gosysoppgave feilet, cronjob vil forsøke på nytt", exc)
                         }
-                        vedtakService.sendVedtakToInfotrygd(vedtak = newVedtak)
-                        delay(1000) // Wait for infotrygd kvittering to be consumed
-
-                        val vedtak = vedtakService.getVedtak(uuid = newVedtak.uuid)
-                        val response = VedtakResponseDTO.createFromVedtak(vedtak = vedtak)
-                        log.info("Created vedtak with infotrygd status: ${response.infotrygdStatus}, isJournalfort: ${response.isJournalfort}, hasGosysOppgave: ${response.hasGosysOppgave}")
-                        call.respond(HttpStatusCode.Created, response)
                     }
+                    vedtakService.sendVedtakToInfotrygd(vedtak = newVedtak)
+                    delay(1000) // Wait for infotrygd kvittering to be consumed
+
+                    val vedtak = vedtakService.getVedtak(uuid = newVedtak.uuid)
+                    val response = VedtakResponseDTO.createFromVedtak(vedtak = vedtak)
+                    log.info("Created vedtak with infotrygd status: ${response.infotrygdStatus}, isJournalfort: ${response.isJournalfort}, hasGosysOppgave: ${response.hasGosysOppgave}")
+                    call.respond(HttpStatusCode.Created, response)
                 }
             }
         }
 
         put(ferdigbehandlingPath) {
-            val action = "update ferdigbehandling for person"
-
             checkPersonAndSyfoTilgang(
-                action = action,
+                action = "update ferdigbehandling for person",
                 tilgangskontrollClient = tilgangskontrollClient,
                 requiresWriteAccess = true,
-            ) { authorizedContext ->
-                with(authorizedContext) {
-                    val personident = Personident(personIdent.value)
-                    val vedtakUUID = UUID.fromString(call.parameters[vedtakUUIDParam])
+            ) { authorizedUser, targetPersonIdent, _ ->
+                val personident = Personident(targetPersonIdent.value)
+                val vedtakUUID = UUID.fromString(
+                    requireNotNull(call.parameters[vedtakUUIDParam]) { "Missing vedtakUUID" }
+                )
 
-                    val vedtak = vedtakService.getVedtak(personident).firstOrNull { it.uuid == vedtakUUID }
-                    if (vedtak == null || vedtak.isFerdigbehandlet()) {
-                        call.respond(HttpStatusCode.BadRequest, "Finner ikke åpent vedtak med uuid=$vedtakUUID")
-                    } else {
-                        val ferdigbehandletVedtak = vedtakService.ferdigbehandleVedtak(
-                            vedtak = vedtak,
-                            veilederident = navIdent.value,
-                        )
-                        call.respond(
-                            HttpStatusCode.OK,
-                            VedtakResponseDTO.createFromVedtak(vedtak = ferdigbehandletVedtak)
-                        )
-                    }
+                val vedtak = vedtakService.getVedtak(personident).firstOrNull { it.uuid == vedtakUUID }
+                if (vedtak == null || vedtak.isFerdigbehandlet()) {
+                    call.respond(HttpStatusCode.BadRequest, "Finner ikke åpent vedtak med uuid=$vedtakUUID")
+                } else {
+                    val ferdigbehandletVedtak = vedtakService.ferdigbehandleVedtak(
+                        vedtak = vedtak,
+                        veilederident = authorizedUser.navIdent.value,
+                    )
+                    call.respond(
+                        HttpStatusCode.OK,
+                        VedtakResponseDTO.createFromVedtak(vedtak = ferdigbehandletVedtak)
+                    )
                 }
             }
         }
